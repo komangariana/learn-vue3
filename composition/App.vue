84b824c2595d7d9b9297da3696829c4ca0de1e77
I Komang Ariana <km.ariana1992@gmail.com>
2020-10-30 13:26:37 +0800

    Create playground page of composition API

---
+++ composition/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="increment">{{ count }}</button>
+  <button @click="a++">{{ a }}</button>
+  <button @click="b++">{{ b }}</button>
+  <div v-for="msg in history">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+import { useNumbers } from './numbers'
+export default {
+  setup() {
+    const {
+      history,
+      increment,
+      count,
+      a,
+      b
+    } = useNumbers()
+    return {
+      history,
+      increment,
+      count,
+      a,
+      b
+    }
+  }
+}
+</script>
