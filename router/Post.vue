1091efe99225c789ef04eb5081f3ad270f86387a
I Komang Ariana <km.ariana1992@gmail.com>
2020-11-21 13:41:20 +0800

    create sample page of hello world and post page

---
+++ router/Post.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-if="post">
+    <h4>{{ post.title }}</h4>
+    <p>
+    {{ post.content }}
+    </p>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useRoute, useRouter } from "vue-router"
+import { testPosts } from '../microblog/testPosts.js'
+export default {
+  beforeRouteEnter(to, from, next) {
+    // if (this.props.auth) {
+    // } else {
+    //   next('/router/posts')
+    // }
+    next()
+  },
+  computed: {
+    post() {
+      return testPosts.find(
+        x => x.id === parseInt(this.$route.params.id)
+      )
+    }
+  },
+  // setup() {
+  //   const route = useRoute()
+  //   console.log(route.params)
+  //   const post = computed(() => {
+  //     return testPosts.find(
+  //       x => x.id === parseInt(route.params.id)
+  //     )
+  //   })
+  //   return {
+  //     post
+  //   }
+  // }
+}
+</script>
---
+++ router/Posts.vue
@@ -0,0 +1,37 @@
+<template>
+  <h4>Posts</h4>
+  <router-link 
+    v-for="post in testPosts"
+    :to="`/router/posts/${post.id}`"
+  >
+    {{ post.title }}
+  </router-link>
+  <router-view />
+</template>
+
+<script>
+import { watchEffect } from 'vue'
+import { testPosts } from '../microblog/testPosts.js'
+import { useRoute } from "vue-router"
+
+export default {
+  // watch: {
+  //   '$route': {
+  //     handler: (val, oldVal) => {
+  //       console.log(val)
+  //       this.fetchData(val.params.id)
+  //     },
+  //     immediate: true
+  //   }
+  // },
+  setup() {
+    const route = useRoute()
+    // watchEffect(() => {
+    //   fetchData(route.params.id)
+    // })
+    return {
+      testPosts
+    }
+  }
+}
+</script>
