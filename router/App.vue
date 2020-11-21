30d3404d17e5596e8c3568d862cd6f085c37c989
I Komang Ariana <km.ariana1992@gmail.com>
2020-11-21 13:40:42 +0800

    setup router test module

---
+++ router/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h3>Hello, and welcome to Vue Router</h3>
+
+  <router-link to="/router/hello">
+    Hello
+  </router-link>
+
+  <router-link to="/router/posts">
+    Posts
+  </router-link>
+
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
